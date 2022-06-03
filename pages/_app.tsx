81a4fc6b087273540056286b98ca89e9ec5ae93e
fPrager <wesayso+github@pm.me>
2022-06-03 15:53:22 +0200

    💄 Layout: add geist ui theme to the layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { GeistProvider, CssBaseline } from '@geist-ui/core'
+import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <GeistProvider>
+      <CssBaseline />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </GeistProvider>
+  )
 }
 
 export default MyApp
